083585b565409804d17739de98b07baf50b101cb
Documenter.jl <documenter@juliadocs.github.io>
2022-03-29 14:11:16 +0000

    build based on 77ce30d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Hyper","category":"page"},{"location":"#Hyper","page":"Home","title":"Hyper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Hyper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Hyper]","category":"page"}]
+}
